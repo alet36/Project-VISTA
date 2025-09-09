e399807ee95e3ddc4968e151052d6d0216403aa7
alet.36 <alejandro.tongchau@gmail.com>
2025-09-09 17:08:50 -0500

    Update main.jsx (test)

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './style.css'   // ✅ this line is important
 
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-
+  </React.StrictMode>,
+)
 
 

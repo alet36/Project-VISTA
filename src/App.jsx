5a8f8b524873371193f5553549e457c13d6be3a7
alet.36 <alejandro.tongchau@gmail.com>
2025-09-09 01:35:28 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+      <h1 className="text-5xl font-bold text-white">Tailwind is Working 🎉</h1>
+    </div>
+  )
+}

b12e12bc80e347d179cae7747749af32f2f35453
alet.36 <alejandro.tongchau@gmail.com>
2025-09-09 01:35:05 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,3 @@
 import './index.css'
 
-export default function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <h1 className="text-5xl font-bold text-white">Tailwind is Working 🎉</h1>
-    </div>
-  )
-}
+

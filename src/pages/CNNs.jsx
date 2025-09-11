dc36530b19bf68b01b25cd93c8fb013ef135c489
alet.36 <alejandro.tongchau@gmail.com>
2025-09-11 13:01:53 -0500

    recolor CNNs.jsx

--- src/pages/CNNs.jsx
+++ src/pages/CNNs.jsx
@@ -63,10 +63,13 @@ export default function CNNs() {
       </aside>
 
       {/* Main content */}
-      <section className="col-span-3">
-        <h1 className="text-3xl font-bold mb-6">Convolutional Neural Networks (CNNs)</h1>
+      <section className="col-span-3 text-gray-800 dark:text-gray-200">
+        <h1 className="text-3xl font-bold mb-6">
+          Convolutional Neural Networks (CNNs)
+        </h1>
         {renderSection()}
       </section>
+
     </div>
   );
 }

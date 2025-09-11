54ec3221a3749a8adb3418a676db7613cb60c183
alet.36 <alejandro.tongchau@gmail.com>
2025-09-10 23:50:46 -0500

    Update Vision.jsx

--- src/pages/Vision.jsx
+++ src/pages/Vision.jsx
@@ -1,5 +1,5 @@
 // pages/Vision.jsx
-import { useState } from "react";
+import React from "react";
 
 export default function Vision() {
   const [activeSection, setActiveSection] = useState("intro");

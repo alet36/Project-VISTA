cd93a0d80b7e1bd7ee7fbcffbc9fb356f585f1ce
alet.36 <alejandro.tongchau@gmail.com>
2025-09-11 19:13:45 -0500

    Update CNNs.jsx

--- src/pages/CNNs.jsx
+++ src/pages/CNNs.jsx
@@ -69,7 +69,7 @@ export default function CNNs() {
         </h1>
       
         <div className="prose prose-gray dark:prose-invert max-w-none">
-          <div className="text-gray-800 dark:text-gray-200">
+          <div className="text-black dark:text-gray-200">
             {renderSection(
             )}
           </div>
